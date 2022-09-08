93ea63e6e3c82a28cc639009b59a7e885dbb1dcf
ricardo <nishimu75618094@gmail.com>
2022-09-09 04:31:08 +0900

    classbase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,9 @@
 コンポーネントテスト２
 
 <x-tests.card title="タイトル２" content="本文２" />
+
+
+<x-test-class-base classBaseMessage="メッセージです" />
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
