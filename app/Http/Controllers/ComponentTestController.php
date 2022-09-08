a79f51d4fd71ab77a392551a64b863e36b8ed240
ricardo <nishimu75618094@gmail.com>
2022-09-09 03:57:45 +0900

    bladecomponent

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 class ComponentTestController extends Controller
 {
     public function showComponent1(){
-        return view('test.component-test1');
+        $message = 'メッセージ';
+
+        return view('tests.component-test1', compact('message'));
     }
     public function showComponent2(){
-        return view('test.component-test2');
+        return view('tests.component-test2');
     }
 }
---
+++ resources/views/tests/component-test2.blade.php
@@ -0,0 +1,6 @@
+<x-tests.app>
+    <x-slot name="header">ヘッドー2 </x-slot>
+コンポーネントテスト２
+
+<x-tests.card title="タイトル２" content="本文２" />
+</x-tests.app>
